f84b45ec22299314eb57bf0295a1604ace603d54
Jacek Kawalec <jokerchocolate@gmail.com>
2020-07-30 08:42:29 +0200

    Added Turn handling for player class

--- main.js
+++ main.js
@@ -30,7 +30,6 @@ class Game {
             console.log(this.Players)
             dealerNode.appendChild(this.Players[0].createPlayer())
             playersNode.appendChild(this.Players[1].createPlayer())
-            console.log('this runs')
         }else {
         this.Players.map((player)=>
         playersNode.appendChild(player.createPlayer()))
@@ -41,6 +40,8 @@ class Game {
 class Dealer {
     constructor(name){
     this.name = name
+    this.isActive = false
+    this.score = 0
     }
 
 
@@ -50,7 +51,7 @@ class Dealer {
         playerName.className = `${name}-player`
         let cards = document.createElement('p')
         cards.innerHTML = `${name}'s cards`
-        cards.className =` ${name}-cards`
+        cards.className =`${name}-cards`
         let score = document.createElement('p')
         score.innerHTML = "00"
         score.className = `${name}-cards`
@@ -76,10 +77,44 @@ class Player extends Dealer{
     constructor(name){
         super()
         this.name = name;
+        this.isActive = false
+        this.score = 0
+        this.passHandle = this.passHandle.bind(this)
     }
 
 
-    
+    hitMeHandle(){
+        console.log('click handled')
+    }
+    passHandle(){
+        console.log('click handled')
+        console.log(this)
+        this.endTurn()
+        this.isActive = false
+
+    }
+
+
+    startTurn(name = this.name) {
+        console.log('turn started')
+        let hitMeButton = document.querySelector(`.${name}-hit-button`)
+        let passButton = document.querySelector(`.${name}-fold-button`)
+
+        hitMeButton.addEventListener('click', this.hitMeHandle)
+        passButton.addEventListener('click', this.passHandle)
+        this.hitMeHandle()
+        this.hitMeHandle()
+    }
+
+    endTurn(name=this.name){
+        console.log('turn ended')
+        let hitMeButton = document.querySelector(`.${name}-hit-button`)
+        let passButton = document.querySelector(`.${name}-fold-button`)
+        
+        hitMeButton.removeEventListener('click', this.hitMeHandle)
+        passButton.removeEventListener('click', this.passHandle)
+    }
+
 }
 
 
@@ -88,4 +123,5 @@ class Player extends Dealer{
 let newgame = new Game(['Jacek'])
 newgame.startGame()
 newgame.setupPlayers()
-
+console.log(newgame.Players)
+newgame.Players[1].startTurn()
