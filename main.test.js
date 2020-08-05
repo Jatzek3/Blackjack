ee796eeea6bcc6013dbf0d0a07cebe820029483c
Jacek Kawalec <jokerchocolate@gmail.com>
2020-08-05 14:07:12 +0200

    initial setup ready

--- main.test.js
+++ main.test.js
@@ -1,3 +1,38 @@
-const { Manager, Game, Deck, Dealer, Player, globals } = require('./main.js');
+let {
+  Manager,
+  Game,
+  Deck,
+  Dealer,
+  Player,
+  validName,
+  actualGame,
+  turnStarted,
+  shuffleReady,
+  playersNode,
+  dealerNode,
+} = require('./main.js');
+const { TestScheduler } = require('jest');
 
-console.log(Manager, Game, Deck, Dealer, Player, globals);
+beforeEach(() => {
+  let manager = new Manager();
+  let newDeck = new Deck();
+  const deckReady = newDeck
+    .getNewDeck()
+    .then(() => {
+      newDeck.shuffle();
+    })
+    .then(() => {
+      manager.initialization();
+    })
+    .then(() => {
+      manager.newGame();
+    })
+    .catch(() => console.log('errpr on '));
+  console.log('setup ready');
+});
+
+const sum = (x, y) => x + y;
+
+test('checking test', () => {
+  expect(sum(1, 2)).toBe(3);
+});
